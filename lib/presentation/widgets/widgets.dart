f2a0d67ebe2e1bd58378ff97c5abe3f6b3f59732
Edson <edisaiassan@gmail.com>
2023-08-18 20:45:45 -0500

    Diseño de BottomNavBar

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,2 +1,3 @@
 export 'package:simple_the_movie_db/presentation/widgets/movies/movies_slideshow.dart';
 export 'package:simple_the_movie_db/presentation/widgets/shared/custom_appbar.dart';
+export 'package:simple_the_movie_db/presentation/widgets/shared/custom_bottom_navigation.dart';
